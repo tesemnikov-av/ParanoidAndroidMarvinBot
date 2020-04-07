656059a04672246f9954db8fd324d9759a5573d9
tesemnikov-av <54743742+tesemnikov-av@users.noreply.github.com>
2020-04-07 13:36:17 +0300

    Update parse_kinopoisk.py

--- CORPUS/parse_kinopoisk.py
+++ CORPUS/parse_kinopoisk.py
@@ -14,70 +14,55 @@ page = requests.get("https://www.kinopoisk.ru/top/")
 # a BeautifulSoup object is created. Data is passed to the constructor. The second option refines the parsing object.
 soup = bs(page.content, 'html.parser')
 
-def get_country(url):
-  """
-        This feature gets the movie producer country.
-        This function takes one arguments (url page movie) and returns one values: name country.
-    Example Output:
-        США
-  """
+def get_info(url):
   response = requests.get(url, headers=headers)
   soup = bs(response.text, 'html.parser')
+  info = []
   for i in soup.find_all('table', {'class': 'info'}):
     for j in i.findAll('a')[1]:
-      return(j)
-
-def get_rating_ball(url):
-  response = requests.get(url, headers=headers)
-  soup = bs(response.text, 'html.parser')
+      info.append(j)
   for i in soup.find_all('span', {'class': 'rating_ball'}):
-      return(i.text)
-
-def get_overview(url):
-  response = requests.get(url, headers=headers)
-  soup = bs(response.text, 'html.parser')
+      info.append(i.text)
   for i in soup.find_all('div', {'class': 'brand_words film-synopsys'}):
-      return(i.text)
-
-def get_director(url):
-  response = requests.get(url, headers=headers)
-  soup = bs(response.text, 'html.parser')
+    info.append(i.text)
   for i in soup.find_all('td', {'itemprop': 'director'}):
-      return(i.text)
-
-def get_screeplay(url):
+    info.append(i.text)
   tmp = []
-  response = requests.get(url, headers=headers)
-  soup = bs(response.text, 'html.parser')
   for i in soup.find_all('table', {'class': 'info'}):
     for b in i.find_all('tr'):
       tmp.append((b.text))
-  return(str(tmp[4]).replace("сценарий" , ""))
-
-def get_actor(url):
+    info.append(str(tmp[4]).replace("сценарий" , ""))
+  #actors
   tmp = []
-  response = requests.get(url, headers=headers)
-  soup = bs(response.text, 'html.parser')
   for i in soup.find_all('li', {'itemprop': 'actors'})[0:5]:
     for b in i.find_all('a'):
-      tmp.append((b.text))
-  return(str(tmp).strip('[]').replace("'", ""))
+      #tmp.append((b.text))
+      tmp.append(b.text)
+  info.append(tmp)
+    #info.append(str(tmp).strip('[]').replace("'", ""))
+  tmp=[]
+  for i in soup.find_all('a', {'class': 'popupBigImage'}):
+    for b in i.find_all('img'):
+      #print(b)
+      tmp.append((b['src']))
+  info.append(str(tmp).strip('[]'))
+  return info
 
 # сreate an empty list
 b = []
+count=0
 
 # here i find the name, year and country of the film
 for ultag in soup.find_all('table', {'class': 'js-rum-hero'} ):
-  for i in ultag.find_all('td' , {'style': 'height: 27px; vertical-align: middle; padding: 6px 30px 6px 0'} ): #[0:1:1]:
+  for i in ultag.find_all('td' , {'style': 'height: 27px; vertical-align: middle; padding: 6px 30px 6px 0'} ):
     for j in i.find_all('a'):
       a = ((j.text).replace(')', '').split('('))
-      a.append(get_country(str('https://www.kinopoisk.ru' + j['href'])))
-      a.append(get_rating_ball(str('https://www.kinopoisk.ru' + j['href'])))
-      a.append(get_overview(str('https://www.kinopoisk.ru' + j['href'])))
-      a.append(get_director(str('https://www.kinopoisk.ru' + j['href'])))
-      a.append(get_screeplay(str('https://www.kinopoisk.ru' + j['href'])))
-      a.append(get_actor(str('https://www.kinopoisk.ru' + j['href'])))
+      my_list = get_info(str('https://www.kinopoisk.ru' + j['href']))
+      for i in my_list:
+        a.append(i)
       b.append(a)
+      count=count+1
+      print(count)
 
 # convert my list to Pandas DataFrame
 df = pd.DataFrame(b)
